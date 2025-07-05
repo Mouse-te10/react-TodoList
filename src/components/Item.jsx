404abeea6349e01cbba92b56a08f931d539cb265
Bogdan Matmusaev <bogdanmatmusave@gmail.com>
2025-07-05 21:12:08 +0500

    update without bugs for checkbox

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,18 +1,12 @@
 import React from 'react';
 import { Card, Checkbox, Button } from 'antd';
-import { useState } from 'react';
 const App = (props) => {
-    const [checked, setChecked] = useState(false)
-
-    function checkedly() {
-        setChecked(prev => !prev)
-    }
     return (
         <div>
             {props.arrTasks.map(item => (
-                <Card style={{ width: 500, backgroundColor: checked ? 'rgb(169, 228, 151)' : '#f4f4f4', marginTop: '30px', border: checked ? '1px solid #72ee4c' : ''}}>
+                <Card style={{ width: 500, backgroundColor: '#f4f4f4', marginTop: '30px'}}>
                     <p>{item}</p>
-                    <Checkbox onChange={checkedly}>Выполнено</Checkbox>
+                    <Checkbox>Выполнено</Checkbox>
                     <Button danger style={{marginLeft: '30px'}} onClick={() => props.deleteTask(item)}>
                         Удалить
                     </Button>
